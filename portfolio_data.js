70281a60925246c2edad22d91e7d70f8404e8c85
0xStackedS0ul <128752239+0xStackedS0ul@users.noreply.github.com>
2026-01-03 13:14:29 +0200

    Add files via upload

---
+++ portfolio_data.js
@@ -0,0 +1,52 @@
+window.portfolioData = [
+    {
+        "id": "b4e5777e91984bfc87933e44c1265815",
+        "title": "Hazzard Container",
+        "description": "roblox username:illia2007\n\ndiscord:filexzz",
+        "viewCount": 32,
+        "faceCount": 2912,
+        "modelUrl": "https://sketchfab.com/3d-models/none-b4e5777e91984bfc87933e44c1265815",
+        "image": "https://media.sketchfab.com/models/b4e5777e91984bfc87933e44c1265815/thumbnails/fb3a4a516e5d468a9e1402e669adaf3f/a6d45b73970b4506beaab1d19f45e580.jpeg",
+        "category": "chad"
+    },
+    {
+        "id": "a051ab9d6f9b4b9caef00233233caf02",
+        "title": "Metalic Refridgerator",
+        "description": "Made this one for fun\n\n\nRoblox Username:illia2007\n\nDiscord:filexzz",
+        "viewCount": 22,
+        "faceCount": 2110,
+        "modelUrl": "https://sketchfab.com/3d-models/none-a051ab9d6f9b4b9caef00233233caf02",
+        "image": "https://media.sketchfab.com/models/a051ab9d6f9b4b9caef00233233caf02/thumbnails/f649c87984444bf1b8c9b823b0ff6609/35f3c05c4ee04a08a5d983313dc64ae2.jpeg",
+        "category": "chad"
+    },
+    {
+        "id": "49d7c62059bf497f9f507319259a4662",
+        "title": "Metalic Lootcrate",
+        "description": "Roblox Username :illia2007\nDIscord: filexzz",
+        "viewCount": 47,
+        "faceCount": 3834,
+        "modelUrl": "https://sketchfab.com/3d-models/none-49d7c62059bf497f9f507319259a4662",
+        "image": "https://media.sketchfab.com/models/49d7c62059bf497f9f507319259a4662/thumbnails/9bc60ae578df43629fbdc62be282f18f/dfd905dec6554864932322be4a3f2d4a.jpeg",
+        "category": "chad"
+    },
+    {
+        "id": "eb77599908984a2baef58afc95bce1bb",
+        "title": "Dunegun",
+        "description": "Model made by me. Made for commission\n\n\n\n\n\nUsername:illia2007\ndiscord:filexzz",
+        "viewCount": 15,
+        "faceCount": 6872,
+        "modelUrl": "https://sketchfab.com/3d-models/none-eb77599908984a2baef58afc95bce1bb",
+        "image": "https://media.sketchfab.com/models/eb77599908984a2baef58afc95bce1bb/thumbnails/6b38c32f56b14d1383f9e011b5584fab/1b3bf4ad5f6d4d88ba061c461016afa1.jpeg",
+        "category": "chad"
+    },
+    {
+        "id": "5f14a848bedd40408c06812d7509a136",
+        "title": "Hollowknight",
+        "description": "My model i used for my render that i published on ArtStation\nHollow knight giga fan\n\n\n\nRoblox username: illia2007\ndiscord:filexzz",
+        "viewCount": 35,
+        "faceCount": 7004,
+        "modelUrl": "https://sketchfab.com/3d-models/none-5f14a848bedd40408c06812d7509a136",
+        "image": "https://media.sketchfab.com/thumbnails/4c57687b657445e1b46c83b35e494ad4/1024.jpg",
+        "category": "chad"
+    }
+];
